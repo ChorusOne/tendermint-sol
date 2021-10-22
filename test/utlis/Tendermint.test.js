46f072a076c78aa4da615a7125b5d0b447ea37a7
Kaczanowski Mateusz <kaczanowski.mateusz@gmail.com>
2021-10-22 00:37:22 +0000

    add tendermint related functions

--- test/utlis/Tendermint.test.js
+++ test/utlis/Tendermint.test.js
@@ -22,4 +22,46 @@ contract('TendermintMock', () => {
       assert.equal(hash, expectedHash, 'invalid signed header hash, expected: ' + expectedHash)
     })
   })
+
+  it('verifies total voting power', async () => {
+    const mock = await TendermintMock.deployed()
+    const root = new protobuf.Root()
+
+    await root.load('./proto/TendermintLight.proto', { keepCase: true }).then(async function (root, err) {
+      if (err) { throw err }
+
+      const ValidatorSet = root.lookupType('tendermint.light.ValidatorSet')
+      const validatorSetObj = require('../data/header.28.validator_set.json')
+      const vs = ValidatorSet.fromObject(validatorSetObj)
+
+      const encoded = await ValidatorSet.encode(vs).finish()
+      const votingPower = await mock.totalVotingPower.call(encoded)
+
+      assert.equal(votingPower.toNumber(), 100000, 'invalid voting power')
+    })
+  })
+
+  it('verifies filtering validator set by address', async () => {
+    const mock = await TendermintMock.deployed()
+    const root = new protobuf.Root()
+
+    await root.load('./proto/TendermintLight.proto', { keepCase: true }).then(async function (root, err) {
+      if (err) { throw err }
+
+      const ValidatorSet = root.lookupType('tendermint.light.ValidatorSet')
+      const validatorSetObj = require('../data/header.28.validator_set.json')
+      const vs = ValidatorSet.fromObject(validatorSetObj)
+
+      const encoded = await ValidatorSet.encode(vs).finish()
+      var { 0: index, 1: found } = await mock.getByAddress.call(encoded, vs.validators[0].address)
+
+      assert.equal(index.toNumber(), 0, 'invalid index')
+      assert.equal(found, true, 'invalid search result')
+
+      var { 0: index, 1: found } = await mock.getByAddress.call(encoded, Buffer.from([0x1, 0x2, 0x3]))
+
+      assert.equal(index.toNumber(), 0, 'invalid index')
+      assert.equal(found, false, 'invalid search result')
+    })
+  })
 })
